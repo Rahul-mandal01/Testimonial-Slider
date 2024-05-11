cb6c48384a8c41221c5d65205e25c8397edc89d9
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-11 10:04:23 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,41 @@
+const reviews = [
+    {
+        id: 1,
+        name: "Rahul Kumar Mandal",
+        job: "Software Engineer",
+        image: "src/assets/rahul.jpg",
+        text: "Throughout our collaboration, I've witnessed this team's remarkable technical prowess and unwavering commitment to excellence. Their ability to consistently deliver high-quality solutions, on schedule and within budget, is truly commendable. They've proven themselves to be indispensable partners, elevating every project they undertake.",
+    },
+
+    {
+        id: 2,
+        name: "Taylor Brown",
+        job: "Graphic Designer",
+        image: "./assets/graphicDesigner.jpeg",
+        text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+    },
+    {
+        id: 3,
+        name: "Jordan Smith",
+        job: "Marketing Manager",
+        image: "./assets/marketingManager.jpeg",
+        text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+    },
+    {
+        id: 4,
+        name: "Morgan Kim",
+        job: "Content Strategist",
+        image: "./assets/contentStrategist.jpeg",
+        text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+    },
+    {
+        id: 5,
+        name: "Riley Davis",
+        job: "Data Analyst",
+        image: "./assets/dataAnalyst.jpeg",
+        text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+    },
+];
+
+
+export default reviews;
