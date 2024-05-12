911457b2441a21a5125a930db72b3b527252f356
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-12 22:18:36 +0530

    Create Card.js

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Card = (props) => {
+    let reviews = props.reviews;
+  return (
+    <div>
+        <div>
+            <img src="" alt=""  ></img>
+        </div>
+
+        <div>
+            <p></p>
+        </div>
+
+        <div>
+            <p></p>
+        </div>
+    </div>
+
+  )
+}
+
+export default Card;
