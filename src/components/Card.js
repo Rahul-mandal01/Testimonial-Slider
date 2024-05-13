902eabedb6209aef14f2ac59917a93d286035c3c
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-14 00:14:36 +0530

    updated CSS properties

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,54 @@
 import React from 'react'
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+
+
 
 const Card = (props) => {
-    let reviews = props.reviews;
+    let review = props.review;
   return (
     <div>
-        <div>
-            <img src="" alt=""  ></img>
+        <div className=' rounded-full object-cover w-[140px] h-[140px] z-40 sticky  '>
+            <img src={`${process.env.PUBLIC_URL} ${review.image}`} alt="None" />
+        </div>
+
+        <div className='font-bold text-lg md:text-2xl tracking-wider capitalize flex mx-auto justify-center' >
+            <p>{review.name}</p>
+        </div>
+
+        <div className='text-violet-300 uppercase text-sm flex mx-auto justify-center' >
+            <p>{review.job}</p>
+        </div>
+
+        <div className='text-[#a78bfa] flex mx-auto justify-center'>
+            <FaQuoteLeft/>
+        </div>
+
+        <div className='text-center text-slate-500 mt-4'>
+            {review.text}
+        </div>
+
+        <div className='text-[#a78bfa] flex mx-auto justify-center'>
+            <FaQuoteRight/>
         </div>
 
         <div>
-            <p></p>
+            <button>
+                
+            </button>
         </div>
 
-        <div>
-            <p></p>
+        <div className="text-[#a78bfa] hover:text-[#8b5cf6]" >
+            <button>   + </button>
         </div>
+
+        <div className='bg-[#a78bfa] mt-5, bg-violet-400, hover:bg-violet-500, shadow-md, hover:shadow, transition-all, duration-200, cursor-pointer, px-10, py-2, rounded-md, font-bold, text-white, text-lg'>
+            <button>
+                Surprise me
+            </button>
+        </div>
+
+
+
     </div>
 
   )
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import Card from './Card.js'
+import Card from './Card.js';
 
-export const Testimonials = () => {
+
+const Testimonials = (props) => {
+  let reviews = props.reviews;
   return (
-    <div>
-        <Card></Card>
+    <div className="w-[85vw] md:w-[700px] bg-white mt-10
+    shadow-md hover:shadow-xl rounded-md transition-all 
+    duration-700 flex flex-col justify-center items-center p-10 ">
+        <Card review = {reviews[1]} ></Card>
     </div>
   )
 }
+
+export default Testimonials;
