00f66474e44a61b7f0faa308a1bdeccc0905d8e2
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-14 00:13:23 +0530

    Update CSS properties

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import { Testimonials } from './components/Testimonials';
+import Testimonials from './components/Testimonials';
 import reviews from "./data"
+
 
 function App() {
   return (
-    <div>
-      <h1>Our Testimonials</h1>
-      <div></div>
+    <div className='flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200 '>
+      <h1 className="text-[#000000] text-4xl text-center font-bold flex flex-wrap mx-auto justify-center   " >Our Testimonials</h1>
+      <div className=" bg-violet-400 h-[4px] w-1/2 mt-1 mx-auto "></div>
       <Testimonials reviews={reviews} />
     </div>
   );
