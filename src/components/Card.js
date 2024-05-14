cbddf3327be720db2c27d26af6e6c0d47bab1be6
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-14 15:34:07 +0530

    Added CSS

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,47 +6,33 @@ import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 const Card = (props) => {
     let review = props.review;
   return (
-    <div>
-        <div className=' rounded-full object-cover w-[140px] h-[140px] z-40 sticky  '>
-            <img src={`${process.env.PUBLIC_URL} ${review.image}`} alt="None" />
+    <div className='flex flex-col md:relative'>
+
+        <div className="relative md:absolute md:top-[-7rem] z-[10] mx-auto ">
+            <img className='aspect-square rounded-full object-cover w-[140px] h-[140px] z-40 sticky'
+             src={`${process.env.PUBLIC_URL} ${review.image}`} alt="None" />
+             <div className='w-[140px] h-[140px] bg-violet-500 rounded-full absolute
+             top-[-6px] z-[-5] left-[10px] ' ></div>
         </div>
 
-        <div className='font-bold text-lg md:text-2xl tracking-wider capitalize flex mx-auto justify-center' >
-            <p>{review.name}</p>
+        <div className=' text-center mt-7' >
+            <p className='font-bold text-lg capitalize  md:text-2xl tracking-wider '>{review.name}</p>
+            <p className='text-violet-300 uppercase text-sm' >{review.job}</p>
+
         </div>
 
-        <div className='text-violet-300 uppercase text-sm flex mx-auto justify-center' >
-            <p>{review.job}</p>
-        </div>
 
-        <div className='text-[#a78bfa] flex mx-auto justify-center'>
+        <div className='text-violet-400 mx-auto mt-5'>
             <FaQuoteLeft/>
         </div>
 
-        <div className='text-center text-slate-500 mt-4'>
+        <div className='text-center text-slate-500 mt-4 '>
             {review.text}
         </div>
 
-        <div className='text-[#a78bfa] flex mx-auto justify-center'>
+        <div className='text-violet-400 mx-auto mt-5'>
             <FaQuoteRight/>
         </div>
-
-        <div>
-            <button>
-                
-            </button>
-        </div>
-
-        <div className="text-[#a78bfa] hover:text-[#8b5cf6]" >
-            <button>   + </button>
-        </div>
-
-        <div className='bg-[#a78bfa] mt-5, bg-violet-400, hover:bg-violet-500, shadow-md, hover:shadow, transition-all, duration-200, cursor-pointer, px-10, py-2, rounded-md, font-bold, text-white, text-lg'>
-            <button>
-                Surprise me
-            </button>
-        </div>
-
 
 
     </div>
